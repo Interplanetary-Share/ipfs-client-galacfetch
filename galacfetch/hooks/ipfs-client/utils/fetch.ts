1d04ceb46d4a7662f07bc4167260b043a797f549
kenneth3292922 <kenneth@unionavatars.com>
2023-09-17 03:03:43 +0200

    :construction: replace axios by fetch

---
+++ galacfetch/hooks/ipfs-client/utils/fetch.ts
@@ -0,0 +1,224 @@
+import { IFileUploadResponse } from '../types'
+import { useRemoteIpfsClient } from '../useRemoteIpfsClient'
+
+export const getServers = async (serverGetHost: string): Promise<any> => {
+  const { api } = useRemoteIpfsClient.getState()
+  try {
+    const response = await fetch(serverGetHost, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${api}`,
+      },
+    })
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+
+    const data = await response.json()
+    return data
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
+export const checkIntegrityFile = async (
+  serverCheck: string,
+  fileInfo: { serverAlias: string },
+  cid: string
+): Promise<boolean> => {
+  const { api } = useRemoteIpfsClient.getState()
+
+  try {
+    const url = `${serverCheck}/${fileInfo.serverAlias}/${cid}`
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${api}`,
+      },
+    })
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+
+    const data = await response.json()
+    return data as boolean
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
+export const getFileInfo = async (
+  fileApi: string,
+  cid: string
+): Promise<any> => {
+  const { api } = useRemoteIpfsClient.getState()
+
+  try {
+    const url = `${fileApi}/${cid}`
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${api}`,
+      },
+    })
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+
+    const data = await response.json()
+    return data
+  } catch (err) {
+    console.log(`fastlog => err:`, err)
+  }
+}
+
+export const getAllFilesInfo = async (
+  getAllFiles: string,
+
+  queryParams: any,
+  isPublic: boolean
+): Promise<any> => {
+  const { api } = useRemoteIpfsClient.getState()
+
+  try {
+    const url = new URL(getAllFiles)
+    const params = { ...queryParams, isPublic }
+    Object.keys(params).forEach((key) =>
+      url.searchParams.append(key, params[key])
+    )
+
+    const response = await fetch(url.toString(), {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${api}`,
+      },
+    })
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+
+    const data = await response.json()
+    return data
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const uploadFile = async (
+  fileUpload: string,
+  formData: FormData,
+
+  isPublic: boolean
+): Promise<IFileUploadResponse> => {
+  const { api } = useRemoteIpfsClient.getState()
+
+  try {
+    const url = new URL(fileUpload)
+    url.searchParams.append('isPublic', isPublic.toString())
+
+    const response = await fetch(url.toString(), {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${api}`,
+      },
+      body: formData,
+    })
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+
+    const data = await response.json()
+
+    return data
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const restoreFileIntegrity = async (
+  restoreIntegrity: string,
+  serverAlias: string,
+  formData: FormData
+): Promise<any> => {
+  const { api } = useRemoteIpfsClient.getState()
+
+  try {
+    const url = `${restoreIntegrity}/${serverAlias.trim()}`
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${api}`,
+      },
+      body: formData,
+    })
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+
+    const data = await response.json()
+    return data
+  } catch (err) {
+    console.log(`fastlog => err:`, err)
+  }
+}
+
+export const getExtraProps = async (
+  getExtraPropsFiles: string,
+  cid: string
+): Promise<any> => {
+  const { api } = useRemoteIpfsClient.getState()
+
+  try {
+    const url = `${getExtraPropsFiles}/${cid}`
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${api}`,
+      },
+    })
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+
+    const data = await response.json()
+    return data
+  } catch (err) {
+    console.error(`fastlog => err:`, err)
+  }
+}
+
+export const updateFileProps = async (
+  fileApi: string,
+  cid: string,
+  fileProps: any
+): Promise<any> => {
+  const { api } = useRemoteIpfsClient.getState()
+
+  try {
+    const url = `${fileApi}/${cid}`
+    const response = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${api}`,
+      },
+      body: JSON.stringify(fileProps),
+    })
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+
+    const data = await response.json()
+    return data
+  } catch (err) {
+    console.log(`fastlog => err:`, err)
+  }
+}
--- galacfetch/hooks/ipfs-client/utils/file.ts
+++ galacfetch/hooks/ipfs-client/utils/file.ts
@@ -16,31 +16,6 @@ export const blobBufferToFile = (blob: Blob, cid: string) => {
   return new File([blob], cid)
 }
 
-export const bytes2MB = (bytes = 0) => {
-  return (bytes / 1024 / 1024).toFixed(2)
-}
-
-export const byteNormalize = (bytes = 0) => {
-  if (bytes === 0) return '0 Byte'
-  const k = 1024
-  const dm = 2 < 0 ? 0 : 2
-  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-  const i = Math.floor(Math.log(bytes) / Math.log(k))
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
-}
-
-export const loadScript = (url: string): Promise<void> => {
-  return new Promise((resolve, reject) => {
-    const script = document.createElement('script') as any
-    script.src = url
-    script.async = true
-    script.onload = resolve
-    script.onerror = reject
-
-    document.body.appendChild(script)
-  })
-}
-
 const defaultChunkSize = 1024 * 1024 * 6 // 6MB
 
 export const chunkBlobToBuffer = async (
