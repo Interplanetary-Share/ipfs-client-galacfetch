bc94720432d77c1004814d00c2aafea897ed5efa
kenneth3292922 <kenneth@unionavatars.com>
2023-09-18 00:29:16 +0200

    :bug: update localhost to a real api

--- galacfetch/hooks/ipfs-client/types/api.ts
+++ galacfetch/hooks/ipfs-client/types/api.ts
@@ -1,14 +1,15 @@
-export const baseApi = 'http://localhost:3010';
+// export const baseApi = 'http://localhost:3010';
+export const baseApi = 'https://api.galacfetch.com/'
 
 // Server
-export const serverApi = baseApi + '/server';
-export const serverGetAlias = serverApi + '/get/alias';
-export const serverGetHost = serverApi + '/get/host';
-export const serverCheck = serverApi + '/check';
+export const serverApi = baseApi + '/server'
+export const serverGetAlias = serverApi + '/get/alias'
+export const serverGetHost = serverApi + '/get/host'
+export const serverCheck = serverApi + '/check'
 
 // File
-export const fileApi = baseApi + '/file';
-export const fileUpload = fileApi + '/upload';
-export const getAllFiles = fileApi + '/files';
-export const getExtraPropsFiles = fileApi + '/extra';
-export const restoreIntegrity = fileApi + '/restore/integrity';
+export const fileApi = baseApi + '/file'
+export const fileUpload = fileApi + '/upload'
+export const getAllFiles = fileApi + '/files'
+export const getExtraPropsFiles = fileApi + '/extra'
+export const restoreIntegrity = fileApi + '/restore/integrity'
