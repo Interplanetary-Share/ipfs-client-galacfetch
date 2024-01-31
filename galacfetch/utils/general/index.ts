c6762e05a67bb32d2336be22bd526950d2cdbd0d
Kenneth Suarez <33601506+polopopeye@users.noreply.github.com>
2024-01-31 16:21:47 +0100

    :sparkles: auto export all from files utils general

--- galacfetch/utils/general/index.ts
+++ galacfetch/utils/general/index.ts
@@ -1,13 +1,2 @@
-export {
-  calculateSizeFromObject,
-  bytesToGB,
-  chunkBlobAsync,
-  fileToBlobUrl,
-  reassembleBlob,
-  blobBufferToFile,
-  reassembleArrayBuffer,
-  blobToArrayBuffer,
-  chunkArrayBuffer,
-} from './src/files'
-
+export * from './src/files'
 export * as apiConstants from './src/api'
